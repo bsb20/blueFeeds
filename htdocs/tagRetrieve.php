aa71e347b7d06fed5d11bbecf31e8a7502dec71e
con4rad <conrad.haynes@hotmail.com>
2013-02-28 18:07:26 -0500

    Update tagRetrieve.php

--- htdocs/tagRetrieve.php
+++ htdocs/tagRetrieve.php
@@ -17,7 +17,6 @@ if($row=mysqli_fetch_array($result)){
     $formattedDate=date("m/d/y",$time);
     }
     $finally.=                       "<li data-theme='d' class='listNote dynamicComment' data-dynamicContent='tagRetrieve' onClick='echoComment()' style='margin: 1%; overflow: visible; white-space: normal;'>
-  					<h1>$title</h1>
 						<p class='note'>$text</p>
                                                 <p class='ui-li-aside'><strong>$formattedDate</strong></p>
                                                     <input type='text' name='TUID' value='$TUID' class='hiddenForm' style='display: none;'>

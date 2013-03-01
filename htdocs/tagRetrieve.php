b8788f496d4b64352e93ffff70a6cd1da3543abd
con4rad <conrad.haynes@hotmail.com>
2013-02-28 19:07:21 -0500

    Update tagRetrieve.php

--- htdocs/tagRetrieve.php
+++ htdocs/tagRetrieve.php
@@ -9,15 +9,31 @@ $SUID=$_SESSION["UUID"];
 $sql = "SELECT * FROM `test`.`tags` WHERE `UUID`='$UUID'";
 $result=$db->query($sql);
 $finally="";
+
 for($i=0; $i<mysqli_num_rows($result); $i++){
 if($row=mysqli_fetch_array($result)){
     $text=$row["text"];
     $TUID=$row["TUID"];
+    $counter=0;
+    $sql2 = "SELECT `CUID` FROM `test`.`tu` WHERE `TUID`='$TUID'"
+    $result=$db->query($sql2);
+    for($i=0; $i< mysqli_num_rows(result); i++){
+    	if($row=mysqli_fetch_array($result)){
+    		
+    		$sql3 = "SELECT * FROM `test`.`comments` WHERE `UUID`='$UUID'"
+    			for($i=0; $i< mysqli_num_rows(result); i++){
+    				if($row=mysqli_fetch_array($result)){
+    					$counter=counter+1;
+    				}
+   			 }
+    	}
+    }
     $time=strtotime($date);
     $formattedDate=date("m/d/y",$time);
     }
     $finally.=                       "<li data-theme='d' class='listNote dynamicComment' data-dynamicContent='tagRetrieve' onClick='echoComment()' style='margin: 1%; overflow: visible; white-space: normal;'>
 						<p class='note'>$text</p>
+						<span class="ui-li-count">counter</span></a>
                                                 <p class='ui-li-aside'><strong>$formattedDate</strong></p>
                                                     <input type='text' name='TUID' value='$TUID' class='hiddenForm' style='display: none;'>
 					</li>";

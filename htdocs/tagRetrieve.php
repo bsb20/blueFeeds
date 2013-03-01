d3b6cd00bda5de74682acb005797c0c7f02ab068
con4rad <conrad.haynes@hotmail.com>
2013-02-28 22:29:03 -0500

    Update tagRetrieve.php

--- htdocs/tagRetrieve.php
+++ htdocs/tagRetrieve.php
@@ -18,9 +18,10 @@ if($row=mysqli_fetch_array($result)){
     $TUID=$row["TUID"];
     }
     $finally.=                       "<li data-theme='d' class='listNote dynamicComment' data-dynamicContent='tagRetrieve' onClick='echoComment()' style='margin: 1%; overflow: visible; white-space: normal;'>
-						<div data-role='controlgroup' data-type='horizontal'  class='noteControl' align='right'>
-                                                <a href='#editcomment' data-role='button' data-theme='b' data-mini='true' data-icon='plus'>Edit</a>
+						<div data-role='controlgroup' data-type='horizontal'  class='noteControl'>
+                                                <a href='#commentspage' data-role='button' data-theme='b' data-mini='true' data-icon='plus'>Edit</a>
 						<h1>$text</h1>
+		   		 		<span class="ui-li-count">2</span></a>
 						</div>
                                         	</li>";
 }

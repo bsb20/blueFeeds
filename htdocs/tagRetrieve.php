19c24a279194b44693a22ad10ea2e85820a4c4ef
con4rad <conrad.haynes@hotmail.com>
2013-03-01 05:20:27 -0500

    Update tagRetrieve.php

--- htdocs/tagRetrieve.php
+++ htdocs/tagRetrieve.php
@@ -23,36 +23,5 @@ if($row=mysqli_fetch_array($result)){
 						</a>
                                         </li>";
 }
-
-$sql = "SELECT `CUID` FROM `test`.`tu` WHERE `TUID`='$TUID'"
-    $result=$db->query($sql);
-    for($i=0; $i< mysqli_num_rows(result); i++){
-    	if($row=mysqli_fetch_array($result)){
-    		
-    		$sql = "SELECT * FROM `test`.`comments` WHERE `UUID`='$UUID'"
-    			$result=$db->query($sql);
-    			for($i=0; $i< mysqli_num_rows(result); i++){
-    				if($row=mysqli_fetch_array($result)){
-    					 $title=$row["title"];
-    $text=$row["text"];
-    $CUID=$row["CUID"];
-    $date=$row["date"];
-    $time=strtotime($date);
-    $formattedDate=date("m/d/y",$time);
-    }
-    $finally.=                       "<li data-theme='d' class='listNote dynamicComment' data-dynamicContent='commentRetrieve' onClick='echoComment()' style='margin: 1%; overflow: visible; white-space: normal;'>
-						<h1>$title</h1>
-						<p class='note'>$text</p>
-						<div data-role='controlgroup' data-type='horizontal'  class='noteControl' align='right'>
-                                                <a href='#editcomment' data-role='button' data-theme='b' data-mini='true' data-icon='plus'>Edit</a>
-						<a href='#viewcomment' data-role='button'  data-theme='b' data-mini='true' data-icon='info'>View</a>						
-						</div>
-                                                <p class='ui-li-aside'><strong>$formattedDate</strong></p>
-                                                    <input type='text' name='CUID' value='$CUID' class='hiddenForm' style='display: none;'>
-					</li>";
-    				}
-   			 }
-    	}
-    }
     echo $finally;
 ?>

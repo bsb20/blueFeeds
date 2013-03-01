8f50580fe09965a9548399de4cd16fa519719a9d
con4rad <conrad.haynes@hotmail.com>
2013-03-01 07:19:05 -0500

    Update tagComments.php

--- htdocs/tagComments.php
+++ htdocs/tagComments.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 $table="`test`.`tu`";
+$table2="`test`.`comments`";
 $db=new mysqli("127.0.0.1","root","devils","test",8889);
 if($db->connect_errno){
     echo "FAILURE";
@@ -9,25 +10,14 @@ $TUID=$_SESSION["TUID"];
 $UUID=$_SESSION["UUID"];
 $finally="";
 
-$sql = "SELECT * FROM ".$table." WHERE `TUID`='".$TUID."';";
+$sql = "SELECT * FROM ".$table." JOIN ".$table2." ON ($table.CUID = $table2.CUID) WHERE `TUID`='".$TUID."';";
 $result=$db->query($sql);
     for($i=0; $i<mysqli_num_rows($result); $i++){
         if($row=mysqli_fetch_array($result)){
-            $CUID=$row["CUID"];
-            
-		echo "We have CUID";
-		
-                $table2="`test`.`comments`";
-                $sql2 = "SELECT * FROM ".$table2." WHERE `CUID`='".$CUID."';";	
-                $result2=$db->query($sql2);
-                for($i=0; $i<mysqli_num_rows($result2); $i++){
-                   if($row=mysqli_fetch_array($result2)){
                             $title=$row["title"];
                             $text=$row["text"];
                             $CUID=$row["CUID"];
                             $date=$row["date"];
-                    }
-                }
         }
 $finally.=        "<li data-theme='d' class='listNote dynamicComment' data-dynamicContent='commentRetrieve' onClick='echoComment()' style='margin: 1%; overflow: visible; white-space: normal;'>
     				      <h1>$title</h1>

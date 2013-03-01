3572713159d4de4c62333c0d352552a7201ce7de
con4rad <conrad.haynes@hotmail.com>
2013-02-28 20:47:15 -0500

    Update tagRetrieve.php

--- htdocs/tagRetrieve.php
+++ htdocs/tagRetrieve.php
@@ -1,31 +1,35 @@
 <?php
 session_start();
-$table="`test`.`tags`";
-echo "start";
+$table="`test`.`comments`";
 $db=new mysqli("127.0.0.1","root","devils","test",8889);
 if($db->connect_errno){
     echo "FAILURE";
 }
-$SUID=$_SESSION["UUID"];
-$sql = "SELECT * FROM `test`.`tags` WHERE `UUID`='$UUID'";
+$SUID=$_SESSION["SUID"];
+$sql = "SELECT * FROM `test`.`comments` WHERE `SUID`='$SUID'";
 $result=$db->query($sql);
 $finally="";
-echo "before";
 for($i=0; $i<mysqli_num_rows($result); $i++){
 if($row=mysqli_fetch_array($result)){
-	echo "after";
+    $title=$row["title"];
     $text=$row["text"];
-    $TUID=$row["TUID"];
+    $CUID=$row["CUID"];
+    $date=$row["date"];
     $time=strtotime($date);
     $formattedDate=date("m/d/y",$time);
     }
-    $finally.=                       "<li data-theme='d' class='listNote dynamicComment' data-dynamicContent='tagRetrieve' onClick='echoComment()' style='margin: 1%; overflow: visible; white-space: normal;'>
+    $finally.=                       "<li data-theme='d' class='listNote dynamicComment' data-dynamicContent='commentRetrieve' onClick='echoComment()' style='margin: 1%; overflow: visible; white-space: normal;'>
+						<h1>$title</h1>
 						<p class='note'>$text</p>
-						<span class="ui-li-count">counter</span></a>
+						<div data-role='controlgroup' data-type='horizontal'  class='noteControl' align='right'>
+                                                <a href='#editcomment' data-role='button' data-theme='b' data-mini='true' data-icon='plus'>Edit</a>
+						<a href='#viewcomment' data-role='button'  data-theme='b' data-mini='true' data-icon='info'>View</a>						
+						</div>
                                                 <p class='ui-li-aside'><strong>$formattedDate</strong></p>
-                                                    <input type='text' name='TUID' value='$TUID' class='hiddenForm' style='display: none;'>
+                                                    <input type='text' name='CUID' value='$CUID' class='hiddenForm' style='display: none;'>
 					</li>";
 }
     echo $finally;
 ?>
+
 

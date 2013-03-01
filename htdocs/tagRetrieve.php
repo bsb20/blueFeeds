dc2c05025829c8dc4cc3dd07de7fc3d9ff84343f
con4rad <conrad.haynes@hotmail.com>
2013-02-28 23:41:44 -0500

    Update tagRetrieve.php

--- htdocs/tagRetrieve.php
+++ htdocs/tagRetrieve.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 $table="`test`.`tags`";
-echo "start";
 $db=new mysqli("127.0.0.1","root","devils","test",8889);
 if($db->connect_errno){
     echo "FAILURE";
@@ -10,10 +9,8 @@ $UUID=$_SESSION["UUID"];
 $sql = "SELECT * FROM `test`.`tags` WHERE `UUID`='$UUID'";
 $result=$db->query($sql);
 $finally="";
-echo "before";
 for($i=0; $i<mysqli_num_rows($result); $i++){
 if($row=mysqli_fetch_array($result)){
-	echo "after";
     $text=$row["text"];
     $TUID=$row["TUID"];
     }

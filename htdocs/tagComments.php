0da3914bcf1774c864bd13c992dc8e74dc2ea643
con4rad <conrad.haynes@hotmail.com>
2013-03-01 07:07:37 -0500

    Update tagComments.php

--- htdocs/tagComments.php
+++ htdocs/tagComments.php
@@ -8,8 +8,15 @@ if($db->connect_errno){
 $TUID=$_SESSION["TUID"];
 $UUID=$_SESSION["UUID"];
 $finally="";
+
+$sql = "SELECT * FROM ".$table." WHERE `TUID`='".$TUID."';";
+$result=$db->query($sql);
+    for($i=0; $i<mysqli_num_rows($result); $i++){
+        if($row=mysqli_fetch_array($result)){
+            $CUID=$row["CUID"];
+
                 $table2="`test`.`comments`";
-                $sql2 = "SELECT * FROM ".$table2." WHERE `UUID`='".$UUID."';";	
+                $sql2 = "SELECT * FROM ".$table2." WHERE `CUID`='".$CUID."';";	
                 $result2=$db->query($sql2);
                 for($i=0; $i<mysqli_num_rows($result2); $i++){
                    if($row=mysqli_fetch_array($result2)){
@@ -18,6 +25,8 @@ $finally="";
                             $CUID=$row["CUID"];
                             $date=$row["date"];
                     }
+                }
+        }
 $finally.=        "<li data-theme='d' class='listNote dynamicComment' data-dynamicContent='commentRetrieve' onClick='echoComment()' style='margin: 1%; overflow: visible; white-space: normal;'>
     				      <h1>$title</h1>
 					      	<p class='note'>$text</p>

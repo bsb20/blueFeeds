7f59e6e293a8848f5ae92b3ef0ec959e3ee326a0
con4rad <conrad.haynes@hotmail.com>
2013-02-28 22:22:01 -0500

    Update tagRetrieve.php

--- htdocs/tagRetrieve.php
+++ htdocs/tagRetrieve.php
@@ -19,8 +19,7 @@ if($row=mysqli_fetch_array($result)){
     }
     $finally.=                       "<li data-theme='d' class='listNote dynamicComment' data-dynamicContent='tagRetrieve' onClick='echoComment()' style='margin: 1%; overflow: visible; white-space: normal;'>
 						<a href="#commentspage">
-						<h1>$text</h1>
-						<span class="ui-li-count">12</span></a>
+						<h1>$text</h1></a>
                                         	</li>";
 }
     echo $finally;

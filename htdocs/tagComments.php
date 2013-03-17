ad0b90552957ee47fda0f8ab8b4e355f3bbdab9a
con4rad <conrad.haynes@hotmail.com>
2013-03-17 20:56:23 -0300

    Update tagComments.php

--- htdocs/tagComments.php
+++ htdocs/tagComments.php
@@ -6,7 +6,8 @@ $db=new mysqli("127.0.0.1","root","devils","test",8889);
 if($db->connect_errno){
     echo "FAILURE";
 }
-$TUID=$_SESSION["TUID"];
+//$TUID=$_SESSION["TUID"];
+$TUID=$_POST['rtag'];
 $UUID=$_SESSION["UUID"];
 $finally="";
 

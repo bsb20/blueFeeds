3312103d9f4f2e7f5ce46c6dd34cfe20d22209b8
con4rad <conrad.haynes@hotmail.com>
2013-03-17 19:57:16 -0300

    Update tagRetrieve.php

--- htdocs/tagRetrieve.php
+++ htdocs/tagRetrieve.php
@@ -5,9 +5,8 @@ $db=new mysqli("127.0.0.1","root","devils","test",8889);
 if($db->connect_errno){
     echo "FAILURE";
 }
-$SUID=$_SESSION["SUID"];
 $UUID=$_SESSION["UUID"];
-$sql = "SELECT * FROM `test`.`tags` WHERE `UUID`='$UUID' AND WHERE `SUID`='$SUID'";
+$sql = "SELECT * FROM `test`.`tags` WHERE `UUID`='$UUID'";
 $result=$db->query($sql);
 $finally="";
 for($i=0; $i<mysqli_num_rows($result); $i++){

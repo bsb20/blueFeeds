679baad29ddccc730b1c89c251c0fb2c7774158a
Jzhanglsw <Jzhanglsw@Oracle>
2013-03-21 15:57:23 -0400

    Edited the wrong studentCreate. Reverted.

--- htdocs/desktop/StudentProfile.php
+++ htdocs/desktop/StudentProfile.php
@@ -2,6 +2,7 @@
 	session_start();
 	date_default_timezone_set("America/New_York");
 	$table1="`test`.`students`";
+	$table2="`test`.`appointments`";	
 	$db=new mysqli("127.0.0.1","root","devils","test",8889);
 	if($db->connect_errno){
 		echo "FAILURE";
@@ -17,14 +18,9 @@
 	}
 	
 	date_default_timezone_set("America/New_York");
-	$table2="`test`.`appointments`";
-	$db=new mysqli("127.0.0.1","root","devils","test",8889);
-	if($db->connect_errno){
-		echo "FAILURE";
-	}
 	$sql2 = "SELECT * FROM ".$table2." WHERE `SUID`='$SUID' ORDER BY `start`;";
 	$result2=$db->query($sql2);
-	if($row=mysqli_fetch_array($result1) and $row['isWeekly']==1){
+	if($row=mysqli_fetch_array($result2)){
 
 		$weekly= $row['isWeekly'] ? "Weekly: ".date("l",$start) : date("l, M j", $start);	
 		$start=strtotime($row['start']);

426ce76d334a7ba8401debb88c519f85ebb6347a
Jzhanglsw <Jzhanglsw@Oracle>
2013-03-20 20:37:04 -0400

    123

--- htdocs/desktop/login.php
+++ htdocs/desktop/login.php
@@ -1,24 +1,17 @@
 <?php
-$table1="`test`.`users`";
-$table2="`test`.`su`";
+$table="`test`.`users`";
 $db=new mysqli("127.0.0.1","root","devils","test",8889);
 if($db->connect_errno){
     echo "FAILURE";
 }
 $user=$_POST["usr"];
 $pass=$_POST["pass"];
-$UUID;
-$sql1 = "SELECT * FROM ".$table1." WHERE `user`='".$user."';";
-$result1=$db->query($sql1);
-if($row=mysqli_fetch_array($result1) and $row["pass"]==md5($pass,FALSE)){
+$sql = "SELECT * FROM ".$table." WHERE `user`='".$user."';";
+$result=$db->query($sql);
+if($row=mysqli_fetch_array($result) and $row["pass"]==md5($pass,FALSE)){
         session_start();
         $_SESSION["UUID"]=$row["UUID"];
-		$UUID=$row["UUID"];
-    }
-$sql2 = "SELECT * FROM ".$table2." WHERE `UUID`='".$UUID."';";
-$result2=$db->query($sql2);
-if($row=mysqli_fetch_array($result2))){
-        $_SESSION["SUID"]=$row["SUID"];
+        $_SESSION["SUID"]=513063973e32c;		
 		header('Location: http://bluefeeds.cs.duke.edu/home/htdocs/desktop/test.php');
     }
 

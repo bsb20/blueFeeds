231eef89de3b8be289e51d090953d4fec480a46d
Jzhanglsw <Jzhanglsw@Oracle>
2013-03-21 16:24:26 -0400

    debugging recent appointments

--- htdocs/desktop/StudentProfile.php
+++ htdocs/desktop/StudentProfile.php
@@ -16,27 +16,6 @@
 		$_SESSION["spec"]=$row["specialty"];
 		$_SESSION["email"]=$row["email"];		
 	}
-	
-	date_default_timezone_set("America/New_York");
-	$sql2 = "SELECT * FROM ".$table2." WHERE `SUID`='$SUID' ORDER BY `start`;";
-	$result2=$db->query($sql2);
-	echo "here";
-	for($i=0; $i<mysqli_num_rows($result2); $i++){
-		if($row=mysqli_fetch_array($result2)){
-			if(strtotime($row['start'])>time()){
-				$apptDate= date("l",$start) : date("l, M j", $start);	
-				$start=strtotime($row['start']);
-				$formattedStart=date("g:i",$start);	
-				
-				$_SESSION["nextApptDate"]=$apptDate;		
-				$_SESSION["nextApptTime"]=$formattedStart;	
-			}
-			else{
-				$_SESSION["nextApptDate"]="No upcomig appointments";		
-				$_SESSION["nextApptTime"]="No times scheduled";
-			}
-		}
-	}
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>

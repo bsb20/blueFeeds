af205c2d358826a118eb6ce1e40dbeb9f30b5abc
Jzhanglsw <Jzhanglsw@Oracle>
2013-03-20 20:48:35 -0400

    123

---
+++ htdocs/desktop/test2.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+	session_start();
+	$table="`test`.`su`";
+	$db=new mysqli("127.0.0.1","root","devils","test",8889);
+	if($db->connect_errno){
+		echo "FAILURE";
+	}
+	$UUID=$_SESSION["UUID"];
+	$sql = "SELECT * FROM ".$table." WHERE `UUID`='".$UUID."';";
+	$result=$db->query($sql);
+	if($row=mysqli_fetch_array($result)){
+		$_SESSION["SUID"]=$row["SUID"];
+		echo "success";
+	}
+?>
+</body>
+</html>

dac8f9d34da825cbad830d885b5309dc8a8f7aef
Jzhanglsw <Jzhanglsw@Oracle>
2013-03-22 02:13:03 -0400

    123

--- htdocs/desktop/LandingPage.php
+++ htdocs/desktop/LandingPage.php
@@ -31,7 +31,7 @@ for($i=0; $i<mysqli_num_rows($result); $i++){
 			$today=getDate();
 			$testday = $today['mday'];
 			$day=date("j",$start);
-			$recentAppt.="								<li id='CurrentAppointments'>$name at $formattedStart ++ $testday ++ day</li>";								
+			$recentAppt.="								<li id='CurrentAppointments'>$name at $formattedStart ++ $testday ++ $day</li>";								
 	}
 }
 $_SESSION['appointments'] = $recentAppt;

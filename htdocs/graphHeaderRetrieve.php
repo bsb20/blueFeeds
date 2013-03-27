a80afe5f90b67e614fbeb62191a2a07ea8d39018
con4rad <conrad.haynes@hotmail.com>
2013-03-27 04:15:50 -0300

    Update graphHeaderRetrieve.php

--- htdocs/graphHeaderRetrieve.php
+++ htdocs/graphHeaderRetrieve.php
@@ -5,24 +5,14 @@ $db=new mysqli("127.0.0.1","root","devils","test",8889);
 if($db->connect_errno){
     echo "FAILURE";
 }
-$SUID=$_SESSION["SUID"];
 $UUID=$_SESSION["UUID"];
 $sql = "SELECT * FROM `test`.`tags` WHERE `UUID`='$UUID'";
-echo "isCalled";
 $result=$db->query($sql);
 $finally="";
 if($row=mysqli_fetch_array($result)){
     $text=$row["text"];
     $TUID=$row["TUID"];
     }
-    echo"Work";
-    $finally.= "	
-			<tr>
-							<th scope='row'>$text</th>
-							<td>$4080</td>
-							<td>$6080</td>
-							<td>$6240</td>
-							<td>$3520</td>
-			</tr>"
+    $finally.= ""
     echo $finally;
 ?>

6e4cac1d1f767813f0b320b778bb32dc89781612
Jzhanglsw <Jzhanglsw@Oracle>
2013-03-28 12:44:44 -0400

    New file

---
+++ htdocs/desktop/updaterss.php
@@ -0,0 +1,26 @@
+<?php
+	if (file_exists('http://bluefeeds.cs.duke.edu/home/htdocs/desktop/bluefeedsTest.xml')) {
+		$xml = simplexml_load_file('http://bluefeeds.cs.duke.edu/home/htdocs/desktop/bluefeedsTest.xml');
+ 		$title=$_POST["title"];
+		$link=$_POST["link"];
+		$date=$_POST["date"];		
+		$desc=$_POST["description"];
+		
+		$item = $xml->addChild('item');
+		$item->addChild('title', $title);
+		$item->addChild('link', $link);
+		$item->addChild('date', $date);	
+		$item->addChild('description', $desc);
+		
+		if($xml->asXML('http://bluefeeds.cs.duke.edu/home/htdocs/desktop/bluefeedsTest.xml'))
+		{
+			echo "Success";
+			print_r($xml);			
+		}
+		else
+		{
+			echo "Failure";
+		}
+/* 		echo $sxe->asXML();		 */			
+	}
+?>

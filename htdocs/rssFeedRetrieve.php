6e39a857cc6608622092c9c6a23a4a217f7af809
con4rad <conrad.haynes@hotmail.com>
2013-03-29 04:59:13 -0300

    Update rssFeedRetrieve.php

--- htdocs/rssFeedRetrieve.php
+++ htdocs/rssFeedRetrieve.php
@@ -24,9 +24,10 @@ if($row=mysqli_fetch_array($result)){
 				<p class='note'>$text</p>
                                 <p class='note'><strong>$formattedDate</strong></p>
 					</li>
-					
+				<div data-role="navbar">
 				<a href=$url data-role='button' data-theme='a'>View</a>
-				<a href='#viewcomment' data-role='button'  data-theme='a'>Remove</a>";
+				<a href='#viewcomment' data-role='button'  data-theme='a'>Remove</a>
+				</div>";
 }
     echo $finally;
 ?>

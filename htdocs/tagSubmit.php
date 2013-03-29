beb5b2f1495a6d61657791deebd27583b6ab4846
con4rad <conrad.haynes@hotmail.com>
2013-03-29 09:49:08 -0300

    Update tagSubmit.php

--- htdocs/tagSubmit.php
+++ htdocs/tagSubmit.php
@@ -5,7 +5,6 @@ $db=new mysqli("127.0.0.1","root","devils","test",8889);
 if($db->connect_errno){
     echo "FAILURE";
 }
-echo "heet";
 $title=$_POST["title"];
 $user=$_SESSION["UUID"];
 $TUID=$_POST["TUID"];

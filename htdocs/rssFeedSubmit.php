71b595a892087cf49ab0c65f17c5585d2fce2e95
con4rad <conrad.haynes@hotmail.com>
2013-03-29 04:50:17 -0300

    Update rssFeedSubmit.php

--- htdocs/rssFeedSubmit.php
+++ htdocs/rssFeedSubmit.php
@@ -11,4 +11,5 @@ $user=$_SESSION["UUID"];
 $FUID=$_POST["FUID"];
 $date=date("Y-m-d H:i:s");
 $db->real_query("INSERT INTO ".$table." (`UUID`, `date`, `url`, `FUID`, `title`) VALUES ('$user', '$date', '$url', '$FUID','$title');");
+echo "true";
 ?>

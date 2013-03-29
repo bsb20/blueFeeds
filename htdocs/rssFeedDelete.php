bf5328c7e7e747312b3eb35fe930874ee10248a7
con4rad <conrad.haynes@hotmail.com>
2013-03-29 05:31:52 -0300

    Update rssFeedDelete.php

--- htdocs/rssFeedDelete.php
+++ htdocs/rssFeedDelete.php
@@ -5,6 +5,7 @@ $db=new mysqli("127.0.0.1","root","devils","test",8889);
 if($db->connect_errno){
     echo "FAILURE";
 }
+echo "what";
 $user=$_SESSION["UUID"];
 $student=$_SESSION["SUID"];
 $FUID=$_POST["FUID"];

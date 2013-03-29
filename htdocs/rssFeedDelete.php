e767edb8b6eb676be6980571afde77a8f366ccc7
con4rad <conrad.haynes@hotmail.com>
2013-03-29 05:18:09 -0300

    Create rssFeedDelete.php

---
+++ htdocs/rssFeedDelete.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+$table="`test`.`feeds`";
+$db=new mysqli("127.0.0.1","root","devils","test",8889);
+if($db->connect_errno){
+    echo "FAILURE";
+}
+$user=$_SESSION["UUID"];
+$student=$_SESSION["SUID"];
+$CUID=$_POST["FUID"];
+$date=date("Y-m-d H:i:s");
+$db->real_query("DELETE FROM".$table."WHERE `FUID`='$FUID';");
+echo "true";
+?>

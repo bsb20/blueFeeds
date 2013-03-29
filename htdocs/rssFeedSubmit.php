9d62f58245149ddab59d03f0efc06896156a5c35
con4rad <conrad.haynes@hotmail.com>
2013-03-29 04:39:05 -0300

    Update rssFeedSubmit.php

--- htdocs/rssFeedSubmit.php
+++ htdocs/rssFeedSubmit.php
@@ -5,14 +5,11 @@ $db=new mysqli("127.0.0.1","root","devils","test",8889);
 if($db->connect_errno){
     echo "FAILURE";
 }
-echo "1";
 $url=$_POST["feedUrl"];
 $title=$_POST["feedName"];
 $user=$_SESSION["UUID"];
 $isReleased=0;
-echo "2";
 $FUID=$_POST["FUID"];
 $date=date("Y-m-d H:i:s");
 $db->real_query("INSERT INTO ".$table." (`UUID`, `date`, `url`, `FUID`, `title`) VALUES ('$user', '$date', '$url', '$FUID','$title');");
-echo "true";
 ?>

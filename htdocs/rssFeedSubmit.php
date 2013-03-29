ec9eed6d3c2c6755403ce00b0d7b71dce0da279e
con4rad <conrad.haynes@hotmail.com>
2013-03-29 04:44:42 -0300

    Update rssFeedSubmit.php

--- htdocs/rssFeedSubmit.php
+++ htdocs/rssFeedSubmit.php
@@ -8,7 +8,6 @@ if($db->connect_errno){
 $url=$_POST["feedUrl"];
 $title=$_POST["feedName"];
 $user=$_SESSION["UUID"];
-$isReleased=0;
 $FUID=$_POST["FUID"];
 $date=date("Y-m-d H:i:s");
 $db->real_query("INSERT INTO ".$table." (`UUID`, `date`, `url`, `FUID`, `title`) VALUES ('$user', '$date', '$url', '$FUID','$title');");

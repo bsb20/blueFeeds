d0b77ff7c9062620c7c15397a6560613e74022b0
con4rad <conrad.haynes@hotmail.com>
2013-03-29 03:41:52 -0300

    Create rssFeedRetrieve.php

---
+++ htdocs/rssFeedRetrieve.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+date_default_timezone_set("America/New_York");
+$table="`test`.`comments`";
+$db=new mysqli("127.0.0.1","root","devils","test",8889);
+if($db->connect_errno){
+    echo "FAILURE";
+}
+$UUID=$_SESSION["UUID"];
+$sql = "SELECT * FROM `test`.`feeds` WHERE `UUID`='$UUID' ORDER BY `date` DESC";
+$result=$db->query($sql);
+$finally="";
+for($i=0; $i<mysqli_num_rows($result); $i++){
+if($row=mysqli_fetch_array($result)){
+    $title=$row["title"];
+    $url=$row["url"];
+    $FUID=$row["FUID"];
+    $date=$row["date"];
+    $time=strtotime($date);
+    $formattedDate=date("m/d/y",$time);
+    }
+    $finally.=                       "<li data-theme='d' class='listNote dynamicComment' data-dynamicContent='rssFeedRetrieve' onClick='echoComment()' style='margin: 1%; overflow: visible; white-space: normal;'>
+    				<h1>$title</h1>
+						<p class='note'>$text</p>
+						<div data-role='controlgroup' data-type='horizontal'  class='noteControl' align='right'>
+                    <a href=$url data-role='button' data-theme='b' data-mini='true' data-icon='plus'>Visit</a>			
+						</div>
+                                                <p class='ui-li-aside'><strong>$formattedDate</strong></p>
+					</li>";
+}
+    echo $finally;
+?>

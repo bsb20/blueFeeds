b8897bebae8ad209334ac65c71d939097f21ea07
Jzhanglsw <Jzhanglsw@Oracle>
2013-04-11 14:36:25 -0400

    Syntax

--- htdocs/desktop/StudentSelection.php
+++ htdocs/desktop/StudentSelection.php
@@ -18,7 +18,7 @@ for($i=0; $i<mysqli_num_rows($result); $i++){
 if($row=mysqli_fetch_array($result)){
     $name=$row["user"];
     $photo=$row["photo"];
-	$photo = 'http://bluefeeds.cs.duke.edu/home/htdocs/' + $photo;
+	$photo = "http://bluefeeds.cs.duke.edu/home/htdocs/" . $photo;
     $title=$row["title"];
     $spec=$row["speciality"];
     $SUID=$row["SUID"];
@@ -27,7 +27,7 @@ if($row=mysqli_fetch_array($result)){
 	$html.=           "<li>
 							<a href='$profilelink'>
 								<div class='icon'>
-									<img src='$photo'/>
+									<img src=$photo/>
 								</div>
 								<div class='data'>
 									<h4>$name</h4>

60f24e27830045202e7eb0b01410de5b9a86f572
bsb20 <ben.berg@duke.edu>
2013-04-12 18:42:31 -0300

    Update courseRetriever.php

--- htdocs/courseRetriever.php
+++ htdocs/courseRetriever.php
@@ -6,8 +6,15 @@ $db=new mysqli("127.0.0.1","root","devils","test",8889);
 if($db->connect_errno){
     echo "FAILURE";
 }
+if(isset($_SESSION["UUID"]){
 $UUID=$_SESSION["UUID"];
-$sql = "SELECT * FROM `test`.`courses` WHERE `UUID`='$UUID'";
+$sql = "SELECT * FROM `test`.`courses` WHERE `UUID`='$UUID'";}
+$link="#studentSelection";
+else{
+$SUID=$_SESSION["SUID"];
+$sql="SELECT * FROM `test`.`gs`, `test`.`courses` WHERE `test`.`gs`.`SUID`='$SUID' AND `test`.`courses`.`GUID`=`test`.`gs`.`GUID`;";
+$link="#studentCourse";
+}
 $result=$db->query($sql);
 $finally="";
 for($i=0; $i<mysqli_num_rows($result); $i++){
@@ -17,7 +24,7 @@ if($row=mysqli_fetch_array($result)){
     $GUID=$row["GUID"];
 }
  $finally.=                       "<li data-theme='c' class='dynamicTag' data-dynamicContent='courseRetriever' style='margin: 1%; overflow: visible; white-space: normal;'>
-      			<a href='#studentSelection'>    
+      			<a href='$link'>    
 						<div data-role='controlgroup' data-type='horizontal'  class='noteControl'>
                   <h1>$title</h1>
                       <p>$info</p>

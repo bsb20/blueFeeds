09425feb031da75a98ccecfb2e5432cb9d72ab6f
con4rad <conrad.haynes@hotmail.com>
2013-04-12 06:17:21 -0300

    Update rssFetch.php

--- htdocs/rssFetch.php
+++ htdocs/rssFetch.php
@@ -12,12 +12,16 @@ $user=$_SESSION["UUID"];
 $FUID=$_POST["FUID"];
 
 // YQL query (SELECT * from feed ... ) // Split for readability // Organizes yahoo queries  
+for($i=0; $i<mysqli_num_rows($result); $i++){
+if($row=mysqli_fetch_array($result)){
+    $url=$row["url"];
+}
 $path = "http://query.yahooapis.com/v1/public/yql?q=";  
 $path .= urlencode("SELECT * FROM feed WHERE url='$url'");  
 $path .= "&format=json"; 
-
 $feed = file_get_contents($path, true);
 $feed = json_decode($feed);
+
 $finally.=                       "<li data-theme='c' class='dynamicTag' data-dynamicContent='courseRetriever' style='margin: 1%; overflow: visible; white-space: normal;'>
           		                <div data-role="content">    
                                     <h1>$feed->title</h1>  

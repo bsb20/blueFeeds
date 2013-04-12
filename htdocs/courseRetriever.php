3a7df568d99dd5d45a0edebf748199f5971641ec
con4rad <conrad.haynes@hotmail.com>
2013-04-12 00:29:22 -0300

    Update courseRetriever.php

--- htdocs/courseRetriever.php
+++ htdocs/courseRetriever.php
@@ -7,7 +7,7 @@ if($db->connect_errno){
     echo "FAILURE";
 }
 $UUID=$_SESSION["UUID"];
-$sql = "SELECT * FROM `test`.`courses`";
+$sql = "SELECT * FROM `test`.`courses` WHERE `UUID`='$UUID'";
 $result=$db->query($sql);
  echo "HERE";
 $finally="";

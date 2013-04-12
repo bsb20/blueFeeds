a7780943ef4a4da4df91acd2feb34a51edd2a8b4
con4rad <conrad.haynes@hotmail.com>
2013-04-12 06:37:55 -0300

    Update rssFetch.php

--- htdocs/rssFetch.php
+++ htdocs/rssFetch.php
@@ -25,10 +25,21 @@ $path .= "&format=json";
 $feed = file_get_contents($path, true);
 $feed = json_decode($feed);
 //$description = $feed->description;
+$rss = new DOMDocument();
+    $rss->load('http://wordpress.org/news/feed/');
+	$feed = array();
+	foreach ($rss->getElementsByTagName('item') as $node) {
+		$item = array ( 
+			'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
+			'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
+			'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
+			'date' => $node->getElementsByTagName('pubDate')->item(0)->nodeValue,
+			);
+		array_push($feed, $item);
+	}
 
 
-
-
+}
 echo $finally;
   
 ?>

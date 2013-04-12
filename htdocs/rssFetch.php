ae7fc807a4de94e7e82cd0a367f58add6a15cb33
con4rad <conrad.haynes@hotmail.com>
2013-04-12 07:23:20 -0300

    Update rssFetch.php

--- htdocs/rssFetch.php
+++ htdocs/rssFetch.php
@@ -5,7 +5,6 @@ $db=new mysqli("127.0.0.1","root","devils","test",8889);
 if($db->connect_errno){
     echo "FAILURE";
 }
-echo "PRELOVE";
 $url=$_POST["feedUrl"];
 $title=$_POST["feedName"];
 $UUID=$_SESSION["UUID"];
@@ -19,7 +18,6 @@ for($i=0; $i<mysqli_num_rows($result); $i++){
 if($row=mysqli_fetch_array($result)){
     $url=$row["url"];
 }
-echo "LOVE";
 $finally="";
 //$description = $feed->description;
 $rss = new DOMDocument();

1fdf81b28a82ab90a8db4f415159f02f7213ff2c
con4rad <conrad.haynes@hotmail.com>
2013-04-12 06:12:01 -0300

    Update rssFetch.php

--- htdocs/rssFetch.php
+++ htdocs/rssFetch.php
@@ -18,8 +18,17 @@ $path .= "&format=json";
 
 $feed = file_get_contents($path, true);
 $feed = json_decode($feed);
-
-$date=date("Y-m-d H:i:s");
-$db->real_query("INSERT INTO ".$table." (`feed`, `UUID`, `date`, `url`, `FUID`, `title`) VALUES (`$feed`, '$user', '$date', '$url', '$FUID','$title');");
-echo "true";
+$finally.=                       "<li data-theme='c' class='dynamicTag' data-dynamicContent='courseRetriever' style='margin: 1%; overflow: visible; white-space: normal;'>
+          		                <div data-role="content">    
+                                    <h1>$feed->title</h1>  
+                                    <div> <p> $feed->description </p> </div>  
+                                   </div>
+                      </li>";
+}
+echo $finally;
+  
 ?>
+
+//$date=date("Y-m-d H:i:s");
+//$db->real_query("INSERT INTO ".$table." (`feed`, `UUID`, `date`, `url`, `FUID`, `title`) VALUES (`$feed`, '$user', '$date', '$url', '$FUID','$title');");
+//echo "true";

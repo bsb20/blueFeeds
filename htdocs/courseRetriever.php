fabea0e7d79dad77cd31d2b815736fe30355698c
con4rad <conrad.haynes@hotmail.com>
2013-04-12 04:12:31 -0300

    Update courseRetriever.php

--- htdocs/courseRetriever.php
+++ htdocs/courseRetriever.php
@@ -1,1 +1,14 @@
-//
+<?php
+session_start();
+date_default_timezone_set("America/New_York");
+$table="`test`.`courses`";
+$db=new mysqli("127.0.0.1","root","devils","test",8889);
+if($db->connect_errno){
+    echo "FAILURE";
+}
+$UUID=$_SESSION["UUID"];
+$sql = "SELECT * FROM `test`.`courses` WHERE `UUID`='$UUID'";
+$result=$db->query($sql);
+ echo "HERE";
+$finally="";
+?>

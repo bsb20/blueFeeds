c720387299202ca305a702bfd4d3c3bf223a7bf2
con4rad <conrad.haynes@hotmail.com>
2013-04-12 06:18:06 -0300

    Update rssFetch.php

--- htdocs/rssFetch.php
+++ htdocs/rssFetch.php
@@ -22,12 +22,7 @@ $path .= "&format=json";
 $feed = file_get_contents($path, true);
 $feed = json_decode($feed);
 
-$finally.=                       "<li data-theme='c' class='dynamicTag' data-dynamicContent='courseRetriever' style='margin: 1%; overflow: visible; white-space: normal;'>
-          		                <div data-role="content">    
-                                    <h1>$feed->title</h1>  
-                                    <div> <p> $feed->description </p> </div>  
-                                   </div>
-                      </li>";
+
 }
 echo $finally;
   

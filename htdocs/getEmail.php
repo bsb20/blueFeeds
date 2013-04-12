d2cc7c216fb01afd988410c8117d4cfa8cefeb83
bsb20 <ben.berg@duke.edu>
2013-04-12 19:43:47 -0300

    Update getEmail.php

--- htdocs/getEmail.php
+++ htdocs/getEmail.php
@@ -1,8 +1,5 @@
 <?php
 session_start();
-echo "HELLO";
-echo $_SESSION["UUID"];
-echo isset($_SESSION["SUID"]);
 $table="`test`.`students`";
 $db=new mysqli("127.0.0.1","root","devils","test",8889);
 if($db->connect_errno){

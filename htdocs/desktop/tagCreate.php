8a3bccefea20998fecba209ac0436abe9d49891f
Jzhanglsw <Jzhanglsw@Oracle>
2013-04-14 19:59:53 -0400

    Bug

--- htdocs/desktop/tagCreate.php
+++ htdocs/desktop/tagCreate.php
@@ -2,7 +2,7 @@
 session_start();
 $table="`test`.`tags`";
 $UUID=$_SESSION["UUID"];
-$db=new mysqli("127.0.0.1","root","root","test",8889);
+$db=new mysqli("127.0.0.1","root","devils","test",8889);
 if($db->connect_errno){
     echo "FAILURE";
 }

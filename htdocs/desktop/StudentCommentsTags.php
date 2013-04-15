03b627178b011bda7e340b435b7cae18aa58cd68
Jzhanglsw <Jzhanglsw@Oracle>
2013-04-14 20:13:49 -0400

    Debug

--- htdocs/desktop/StudentCommentsTags.php
+++ htdocs/desktop/StudentCommentsTags.php
@@ -1,39 +1,5 @@
 <?php
 	session_start();
-	$table="`test`.`comments`";
-	$db=new mysqli("127.0.0.1","root","devils","test",8889);
-	if($db->connect_errno){
-		echo "FAILURE";
-	}
-	$SUID=$_SESSION["SUID"];
-	$sql = "SELECT * FROM ".$table."WHERE `SUID`='$SUID' ORDER BY 'date' DESC";
-	$result=$db->query($sql);
-	$commentList="";
-	for($i=0; $i<mysqli_num_rows($result); $i++){
-		if($row=mysqli_fetch_array($result)){
-			$title=$row["title"];
-			$text=$row["text"];
-			$CUID=$row["CUID"];
-			$date=$row["date"];
-			$time=strtotime($date);
-			$formattedDate=date("m/d/y",$time);
-			$editlink = "editComment.php?CUID=" . $CUID;
-			$deletelink = "deleteComment.php?CUID=" . $CUID;
-			
-		}
-		$commentList.="						<li>
-								<a>$title</a>
-								<div>
-									$text
-									<p>
-										$formattedDate
-									</p>
-									<a href=$editlink><button class='bg-color-blueLight'> Edit </button></a>
-									<a href=$deletelink><button class='bg-color-red'> Delete </button></a>																
-								</div>
-							</li>";
-	}
-	$_SESSION["commentList"]=$commentList;	
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>

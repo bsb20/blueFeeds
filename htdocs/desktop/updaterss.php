bb4943701809f0b5305dac98ff4e77a80f7b68e0
Jzhanglsw <Jzhanglsw@Oracle>
2013-04-17 21:54:29 -0400

    Debug

--- htdocs/desktop/updaterss.php
+++ htdocs/desktop/updaterss.php
@@ -25,11 +25,11 @@
 		$bytes = file_put_contents($_SERVER['DOCUMENT_ROOT'].$filepath, $rss);
 		if($bytes)
 		{
-			header('Location: http://bluefeeds.cs.duke.edu/home/htdocs/desktop/RSS Feeds.php?bytes=' . $bytes);			
+			header('Location: http://bluefeeds.cs.duke.edu/home/htdocs/desktop/RSS Feeds.php?success=' . $bytes);			
 		}
 		else
 		{
-			header('Location: http://bluefeeds.cs.duke.edu/home/htdocs/desktop/RSS Feeds.php?bytes=' . $bytes);				
+			header('Location: http://bluefeeds.cs.duke.edu/home/htdocs/desktop/RSS Feeds.php?failure=' . $bytes);				
 		}
 	}
 	else

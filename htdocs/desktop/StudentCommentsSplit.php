f3a23930a423df92e585af9c588a48c22b35c0ac
Jzhanglsw <Jzhanglsw@Oracle>
2013-04-25 00:26:36 -0400

    Bug

--- htdocs/desktop/StudentCommentsSplit.php
+++ htdocs/desktop/StudentCommentsSplit.php
@@ -127,7 +127,7 @@
 							</form>
 					</div>							
 				</div>
-				<iframe style="float: left; padding-left: 2%; width:50%; height:85%;" src="https://crocodoc.com/view/nj5f89A63jHTeaW7MtFNXrnM1n4NDDLj7YbrRbSW97SatrwAVMVAJqloj5Y_x0JACMU1gdDAhJefUnzzQdZzCaU44Ytko2mfm1LVLQ" frameborder="0"></iframe>								
+				<iframe style="float: left; padding-left: 2%; width:48%; height:85%;" src="https://crocodoc.com/view/nj5f89A63jHTeaW7MtFNXrnM1n4NDDLj7YbrRbSW97SatrwAVMVAJqloj5Y_x0JACMU1gdDAhJefUnzzQdZzCaU44Ytko2mfm1LVLQ" frameborder="0"></iframe>								
         </div>
 </body>
 </html>

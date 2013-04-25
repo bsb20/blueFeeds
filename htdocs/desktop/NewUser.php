d7ee2ab81550acc4b3ec32f0ab85c1cdcbd96284
Jzhanglsw <Jzhanglsw@Oracle>
2013-04-24 22:51:04 -0400

    Bug

--- htdocs/desktop/NewUser.php
+++ htdocs/desktop/NewUser.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-/* The New User page accessed from the login page */
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <link href="./Windows Metro Theme/css/modern.css" rel="stylesheet">

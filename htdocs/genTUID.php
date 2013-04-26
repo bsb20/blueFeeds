b1d69e342dd93b1cd1f32aa9d38fdf99a0241fd7
con4rad <conrad.haynes@hotmail.com>
2013-04-26 05:49:23 -0300

    Update genTUID.php

--- htdocs/genTUID.php
+++ htdocs/genTUID.php
@@ -1,4 +1,9 @@
 <?php
+
+/*
+This php script genereates a new Tag ID (TUID) value.
+*/
+
     $TUID=uniqid("",FALSE);
     $line="";
     $line.="<input type='hidden' name='TUID' id='TUID' value='$TUID'>";

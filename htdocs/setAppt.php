44b6600284a16667c69a2bf4a22583beffa00b0a
con4rad <conrad.haynes@hotmail.com>
2013-04-26 06:17:56 -0300

    Update setAppt.php

--- htdocs/setAppt.php
+++ htdocs/setAppt.php
@@ -1,4 +1,10 @@
 <?php
+
+/*
+This php script sets a appointment to the current appointment value key. This is used after a selection of a 
+specific appointment.
+*/
+
 session_start();
 $_SESSION["AUID"]=$_POST["key"];
 ?>

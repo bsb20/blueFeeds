670bda873a0870ae730248cd663acbe5ec166a43
con4rad <conrad.haynes@hotmail.com>
2013-04-26 05:46:33 -0300

    Update genCUID.php

--- htdocs/genCUID.php
+++ htdocs/genCUID.php
@@ -1,4 +1,9 @@
 <?php
+
+/*
+This php script updates the Comment ID (CUID) value. It is called from a jquery function after a comment is selected.
+*/
+
     $CUID=uniqid("",FALSE);
     $line="";
     $line.="<input type='hidden' name='CUID' id='CUID' value='$CUID'>";

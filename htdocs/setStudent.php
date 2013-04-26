4b0cbb21199f30a4e39facdef167a42214a57455
con4rad <conrad.haynes@hotmail.com>
2013-04-26 06:19:11 -0300

    Update setStudent.php

--- htdocs/setStudent.php
+++ htdocs/setStudent.php
@@ -1,5 +1,10 @@
 <?php
 
+/*
+This php script sets a student (SUID) to the current student value key. This is used after a selection of a 
+specific student before advancing to the student profile page.
+*/
+
 session_start();
 $_SESSION["SUID"]=$_POST["key"];
 ?>

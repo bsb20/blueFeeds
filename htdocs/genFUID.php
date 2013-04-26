18a23a05d08ca5d124fa7f6b45059951a7561755
con4rad <conrad.haynes@hotmail.com>
2013-04-26 05:48:59 -0300

    Update genFUID.php

--- htdocs/genFUID.php
+++ htdocs/genFUID.php
@@ -1,4 +1,9 @@
 <?php
+    
+/*
+This php script genereates a new (FUID) value.
+*/
+
     $FUID=uniqid("",FALSE);
     $line="";
     $line.="<input type='hidden' name='FUID' id='FUID' value='$FUID'>";

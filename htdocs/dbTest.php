2475f8a06f0901c15452b3f2f9d2151bdbad1771
con4rad <conrad.haynes@hotmail.com>
2013-04-26 05:45:18 -0300

    Update dbTest.php

--- htdocs/dbTest.php
+++ htdocs/dbTest.php
@@ -1,4 +1,9 @@
 <?php
+
+/*
+This php script submit can be used to verify database connection status. If connected, the script will output post info. 
+*/
+
 $db=new mysqli("127.0.0.1","root","root","test",8889);
 if($db->connect_errno){
     echo "FAILURE";

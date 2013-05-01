cc3f06e85f68cfdeb2dc3c6745e9fd574e997f64
Jzhanglsw <Jzhanglsw@Oracle>
2013-05-01 18:15:40 -0400

    Forgot a word

--- htdocs/desktop/displayAlert.php
+++ htdocs/desktop/displayAlert.php
@@ -3,7 +3,7 @@
 $(document).ready(function(){
         $.Dialog({
             'title'       : 'Please select a course before continuing.',
-            'content'     : 'Course selection can be completed in top right hand corner of this screen. If you are receiving this alert, it is because you have attempted to perform an action without having selected a course such as: adding instructors or navigating to the student selection page.',
+            'content'     : 'Course selection can be completed in the top right hand corner of this screen. If you are receiving this alert, it is because you have attempted to perform an action without having selected a course such as: adding instructors or navigating to the student selection page.',
             'draggable'   : true,
             'overlay'     : true,
             'closeButton' : true,

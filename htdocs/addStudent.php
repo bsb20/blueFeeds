662329a3a55677c62e1b4d2d1f7fbf6fe9cf5396
bsb20 <ben.berg@duke.edu>
2013-05-01 16:39:51 -0300

    Update addStudent.php

--- htdocs/addStudent.php
+++ htdocs/addStudent.php
@@ -1,6 +1,7 @@
 <?php
 
 /*
+Authors: Benjamin Berg, Rachel Harris, Conrad Haynes, Jack Zhang
 This php script is by the main page of the blueFeeds application login page to add a user student profile to out database. 
 Student profiles are created with a new student id and group id. 
 */

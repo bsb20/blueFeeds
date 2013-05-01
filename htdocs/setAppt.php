0230facfcbae135d7ca94769759effac4ce6ac63
bsb20 <ben.berg@duke.edu>
2013-05-01 17:15:37 -0300

    Update setAppt.php

--- htdocs/setAppt.php
+++ htdocs/setAppt.php
@@ -1,6 +1,7 @@
 <?php
 
 /*
+Authors: Benjamin Berg, Rachel Harris, Conrad Haynes, Jack Zhang
 This php script sets a appointment to the current appointment value key. This is used after a selection of a 
 specific appointment.
 */

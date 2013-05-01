227cb53a50a8587f05835dc79868d4278cef8e6d
bsb20 <ben.berg@duke.edu>
2013-05-01 17:18:10 -0300

    Update tagCreate.php

--- htdocs/tagCreate.php
+++ htdocs/tagCreate.php
@@ -1,6 +1,7 @@
 <?php
 
 /*
+Authors: Benjamin Berg, Rachel Harris, Conrad Haynes, Jack Zhang
 This php script allows the user to create a new tag (TUID) and adds it to the database. 
 */
 session_start();

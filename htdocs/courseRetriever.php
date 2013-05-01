f06a0bfab4645f15fcd05126f1edacb980801eb9
bsb20 <ben.berg@duke.edu>
2013-05-01 17:06:04 -0300

    Update courseRetriever.php

--- htdocs/courseRetriever.php
+++ htdocs/courseRetriever.php
@@ -1,6 +1,7 @@
 <?php
 
 /*
+Authors: Benjamin Berg, Rachel Harris, Conrad Haynes, Jack Zhang
 This php script, called within the course selection page, loads all courses for a given user (UUID) and displays them in
 html formatted jquerymobile.
 */

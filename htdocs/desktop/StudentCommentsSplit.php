5c38b857d997e099732f6184ea0e5900a920983d
Jzhanglsw <Jzhanglsw@Oracle>
2013-05-02 00:10:06 -0400

    Debug

--- htdocs/desktop/StudentCommentsSplit.php
+++ htdocs/desktop/StudentCommentsSplit.php
@@ -121,7 +121,7 @@
         <div class="StudentCommentsSplit">
             <h2>
 				<?php 
-					echo $_SESSION['student'] . "'s Comment Page";					
+					echo $_SESSION['student'] . "'s Comment Page " . $_SESSION['GUID'];					
 				?>				
 			</h2>
 				<div style="width:50%;height:85%;line-height:3em;padding:1.5px;overflow-x: hidden; float: left;">

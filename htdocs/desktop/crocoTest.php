7e8332dd01235a84ffea54757de9e5fa062c54de
Jzhanglsw <Jzhanglsw@Oracle>
2013-05-02 22:29:32 -0400

    Test

--- htdocs/desktop/crocoTest.php
+++ htdocs/desktop/crocoTest.php
@@ -4,6 +4,10 @@ $uuids  = "a81968cf-55bd-4133-b961-ceda33f434f2";
 $status = $croco->getStatus($uuids);
 var_dump($status);
 
+$uuid  = "a81968cf-55bd-4133-b961-ceda33f434f2";
+$session = $croco->createSession($uuid);
+var_dump($session);
+
 class Crocodoc { 
     public $api_key = 'V0dDkJz3i64l87CbSFq2EIXm';
     public $api_url = 'https://crocodoc.com/api/v2/';
@@ -16,5 +20,14 @@ class Crocodoc {
         // this is a GET request
         return file_get_contents($url.$dataStr);
     }
+	
+    public function createSession($uuid){
+        $url = $this->api_url.'session/create';
+        $token = $this->api_key;
+        $dataStr = '?token='.$token.'&uuid='.$uuid;
+
+        // this is a GET request
+        return file_get_contents($url.$dataStr);
+    }	
 }
 ?>

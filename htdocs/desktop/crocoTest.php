45790da0f74e41bc7390d0a56c94c2dfd2885dd9
Jzhanglsw <Jzhanglsw@Oracle>
2013-05-02 22:25:16 -0400

    Test

--- htdocs/desktop/crocoTest.php
+++ htdocs/desktop/crocoTest.php
@@ -1,9 +1,20 @@
 <?php
-error_reporting(E_ALL);
-require_once('crocodoc.php');
-$croco = new Crocodoc();
-/* $uuid = $croco->upload('http://bluefeeds.cs.duke.edu/home/htdocs/desktop/uploadsPDF/Faculty%20Eval%20Form%20Fall%202012%20Yr.1.pdf');
- */
-$uuid = $croco->getStatus('a81968cf-55bd-4133-b961-ceda33f434f2'); 
-var_dump($uuid);
+$croco  = new Crocodoc();
+$uuids  = "a81968cf-55bd-4133-b961-ceda33f434f2";
+$status = $croco->getStatus($uuids);
+var_dump($status);
+
+class Crocodoc { 
+    public $api_key = 'V0dDkJz3i64l87CbSFq2EIXm';
+    public $api_url = 'https://crocodoc.com/api/v2/';
+
+    public function getStatus($uuids){
+        $url = $this->api_url.'document/status';
+        $token = $this->api_key;
+        $dataStr = '?token='.$token.'&uuids='.$uuids;
+
+        // this is a GET request
+        return file_get_contents($url.$dataStr);
+    }
+}
 ?>

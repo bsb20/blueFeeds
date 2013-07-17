eadaef1909b5969ccd3a9936ed4e7b1c962c64cc
bsb20 <ben.berg@duke.edu>
2013-07-17 17:11:41 -0400

    Update initialize.php

--- htdocs/initialize.php
+++ htdocs/initialize.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 date_default_timezone_set("America/New_York");
-$db=new mysqli("127.0.0.1","root","devils","test",8889);
+$db=new mysqli("127.0.0.1","root","devils","test",3306);
 if($db->connect_errno){
     echo "FAILURE";
 }

395f691f6dcceec43946c7529cf65def10bb4c78
bsb20 <ben.berg@duke.edu>
2013-07-19 07:40:30 -0400

    Update initialize.php

--- htdocs/initialize.php
+++ htdocs/initialize.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+if(!isset($_SESSION["UUID"])){
+    echo "~"
+    }
 date_default_timezone_set("America/New_York");
 $db=new mysqli("127.0.0.1","root","devils","test",3306);
 if($db->connect_errno){

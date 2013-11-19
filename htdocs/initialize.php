c33d081c1ce77dfa15125fd2505561a420b541ff
BrettTester <brettcadigan@gmail.com>
2013-11-18 22:36:21 -0500

    changed db user to duke

--- htdocs/initialize.php
+++ htdocs/initialize.php
@@ -4,7 +4,7 @@ if(!isset($_SESSION["UUID"]) && !isset($_SESSION["SUID"])){
     echo "~";
     }
 date_default_timezone_set("America/New_York");
-$db=new mysqli("127.0.0.1","root","devils","test",3306);
+$db=new mysqli("127.0.0.1","duke","bluedevils","test",3306);
 if($db->connect_errno){
     echo "FAILURE";
 }

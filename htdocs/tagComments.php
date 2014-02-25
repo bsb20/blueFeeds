adad7036523f9640e6607db1fa78fcffc5ffcbac
Brett <brettcadigan@gmail.com>
2014-02-25 02:06:12 +0000

    added no filter to filter views

--- htdocs/tagComments.php
+++ htdocs/tagComments.php
@@ -29,7 +29,6 @@ $finally.=        "<li data-theme='d' class='listNote dynamicComment' data-dynam
 					      	<p class='note'>$text</p>
 					      	<div data-role='controlgroup' data-mini='true' data-type='horizontal' align='right'>
 						<a href='#editcomment' data-role='button' data-mini='true'>Edit</a>
-						<a href='#viewcomment' data-role='button' data-mini='true'>View</a>
 						</div>
 						<p class='ui-li-aside'><strong>$formattedDate</strong></p>
                                                     <input type='text' name='CUID' value='$CUID' class='hiddenForm' style='display: none;'>
